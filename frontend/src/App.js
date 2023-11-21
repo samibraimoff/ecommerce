b96725e9cb7f9c083b10b473951918f9e8666b68
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2023-11-20 21:51:17 -0500

    add logo, header, footer, custom bootstrap and install prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import Header from './components/header'
-import Footer from './components/footer'
-import {Container} from 'react-bootstrap'
+import React from "react";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
     <>
       <Header />
-      <main>
+      <main className="py-3">
         <Container>
           <h1>Welcome to Ecommerce</h1>
         </Container>
       </main>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import {Col, Row ,Container} from 'react-bootstrap'
-
+import React from "react";
+import { Col, Row, Container } from "react-bootstrap";
 
 const Footer = () => {
-    const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
   return (
     <footer>
-        <Container>
-            <Row>
-                <Col className='text-center py-3'>
-                    <p>Ecommerce &copy; {currentYear}</p>
-                </Col>
-            </Row>
-        </Container>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">
+            <p>Ecommerce &copy; {currentYear}</p>
+          </Col>
+        </Row>
+      </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
