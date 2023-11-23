ca65f45decf91b72c05e9404f70f49942c3a194c
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2023-11-22 22:12:16 -0500

    add backend controller and routes

---
+++ backend/controllers/product-controllers.js
@@ -0,0 +1,19 @@
+import { asyncHandler } from "../middleware/asyncHandler";
+import Product from "../models/product-model";
+
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    return res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
+
+export { getProductById, getProducts };
